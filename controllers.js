3feada46bbbc696899aee536be090d5821cedef1
mariyaKPetkova <90306706+mariyaKPetkova@users.noreply.github.com>
2021-12-03 17:38:02 +0200

    add create

--- controllers.js
+++ controllers.js
@@ -1,11 +1,22 @@
 const router = require('express').Router()
 const caveService = require('./services/caves.js')
-router.get('/',async (req,res)=>{
+
+router.get('/', async (req, res) => {
     const data = await caveService.getAll()
     res.json(data)
 })
-router.get('/:id',async (req,res)=>{
-    const data = await caveService.getOne()
+router.post('/', async (req, res) => {
+    const data = {
+        name: req.body.name,
+        location: req.body.location,
+        description: req.body.description,
+        imageUrl: req.body.imageUrl,
+    }
+    const result = await caveService.create(data)
+    res.status(201).json(result)
+})
+router.get('/:id', async (req, res) => {
+    const data = await caveService.getOne(id)
     res.json(data)
 })
 module.exports = router
--- services/caves.js
+++ services/caves.js
@@ -1,11 +1,18 @@
 const Cave = require('../models/Cave.js')
-async function getAll(){
+
+async function getAll() {
     return Cave.find({}).lean()
 }
-async function getOne(id){
+async function create(data) {
+    const result = new Cave(data)
+    await result.save()
+    return result
+}
+async function getOne(id) {
     return Cave.findById(id).lean()
 }
 module.exports = {
-getAll,
-getOne
+    getAll,
+    getOne,
+    create,
 }
