788d22d2f9d3b2dcdcfc8cbc26755bc8f184548a
mariyaKPetkova <90306706+mariyaKPetkova@users.noreply.github.com>
2021-12-07 11:08:00 +0200

    add guards

--- controllers.js
+++ controllers.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const { isAuht } = require('./middlewares/guards.js')
 const caveService = require('./services/caves.js')
 
 router.get('/', async (req, res) => {
@@ -6,12 +7,13 @@ router.get('/', async (req, res) => {
     const data = await caveService.getAll()
     res.json(data)
 })
-router.post('/', async (req, res) => {
+router.post('/', isAuht(), async (req, res) => {
     const data = {
         name: req.body.name,
         location: req.body.location,
         description: req.body.description,
         imageUrl: req.body.imageUrl,
+        owner: req.user._id
     }
     const result = await caveService.create(data)
     res.status(201).json(result)
--- models/Cave.js
+++ models/Cave.js
@@ -1,10 +1,11 @@
-const {Schema,model} = require('mongoose')
+const { Schema, model } = require('mongoose')
 
 const schema = new Schema({
-name:{type:String},
-location:{type:String},
-description:{type:String},
-imageUrl:{type:String}
+    name: { type: String },
+    location: { type: String },
+    description: { type: String },
+    imageUrl: { type: String },
+    owner:{type: Schema.Types.ObjectId, ref:'User'}
 })
 
-module.exports = model('Cave',schema)
+module.exports = model('Cave', schema)
