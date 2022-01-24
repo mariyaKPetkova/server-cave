766350b87c1776736003f6fe36cb59d089522e9c
mariyaKPetkova <90306706+mariyaKPetkova@users.noreply.github.com>
2022-01-24 18:29:27 +0200

    change port

--- index.js
+++ index.js
@@ -37,5 +37,5 @@ async function start(){
         res.send('hi')
     })
 
-    app.listen(port, () => console.log('Server is listening'))
+    app.listen(port, () => console.log(`Server is listening on port ${port}`))
 }
