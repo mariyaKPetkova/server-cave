525140217d5657b9b4f38f1d94d4d75ddf1ed0f2
mariyaKPetkova <90306706+mariyaKPetkova@users.noreply.github.com>
2022-02-14 19:27:02 +0200

    add procfile and env

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000
 start()
 async function start(){
     await new Promise((resolve,reject)=>{
-        mongoose.connect('mongodb://localhost:27017/caves')
+        mongoose.connect( process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/caves')
         const db = mongoose.connection
         db.once('open',()=>{
             console.log('Database connected');
